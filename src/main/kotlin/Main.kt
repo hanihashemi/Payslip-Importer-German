1202ae8c24f729ca2c30fa79f44cb9adc401c78b
Hani <jhanihashemi@gmail.com>
2025-01-07 05:12:10 +0100

    Import list of pdfs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
 package io.github.hanihashemi.payslipimporter
-
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
 import java.io.File
 import java.util.Scanner
 
 fun main() {
-    // Get the PDF file path from the user
-    val scanner = Scanner(System.`in`)
-    println("Enter the path to your payslip PDF file:")
-    val pdfFilePath = scanner.nextLine()
+    // Get the directory path from the user
+//    val scanner = Scanner(System.`in`)
+//    println("Enter the path to the directory containing your payslip PDF files:")
+//    val directoryPath = scanner.nextLine()
+    val directoryPath = "/Users/h.hashemifar/Downloads/Salary"
+
+    // Get all PDF files in the directory, sorted by name and last modified date
+    val pdfFiles = File(directoryPath).listFiles { file -> file.extension.equals("pdf", ignoreCase = true) }
+        ?.sortedWith(compareBy<File> { it.name })
+        ?: run {
+            println("No PDF files found in the specified directory.")
+            return
+        }
 
     // Define the fields you want to extract
     val fieldsToExtract = listOf(
@@ -23,21 +31,25 @@ fun main() {
         "Gesamtbeitrag zur KV"
     )
 
-    // Read and process the PDF file
-    val extractedData = extractFieldsFromPDF(pdfFilePath, fieldsToExtract)
+    // Process each PDF file
+    pdfFiles.forEach { pdfFile ->
+        println("Processing file: ${pdfFile.name}")
+        val extractedData = extractFieldsFromPDF(pdfFile, fieldsToExtract)
 
-    // Print the extracted data
-    extractedData.forEach { (field, value) ->
-        println("$field: $value")
+        // Print the extracted data
+        extractedData.forEach { (field, value) ->
+            println("$field: $value")
+        }
+        println("-------------------------------------")
     }
 }
 
-fun extractFieldsFromPDF(pdfFilePath: String, fields: List<String>): Map<String, String> {
+fun extractFieldsFromPDF(pdfFile: File, fields: List<String>): Map<String, String> {
     val extractedData = mutableMapOf<String, String>()
 
     try {
         // Load the PDF document
-        PDDocument.load(File(pdfFilePath)).use { document ->
+        PDDocument.load(pdfFile).use { document ->
             // Extract text from the PDF
             val pdfText = PDFTextStripper().getText(document)
 
@@ -50,7 +62,7 @@ fun extractFieldsFromPDF(pdfFilePath: String, fields: List<String>): Map<String,
             }
         }
     } catch (e: Exception) {
-        println("Error reading PDF: ${e.message}")
+        println("Error reading PDF ${pdfFile.name}: ${e.message}")
     }
 
     return extractedData
